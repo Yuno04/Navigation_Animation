f051e6bb80ca92cdd1a3e0dea8e3094b4ddbf0e5
Yuno04 <121325560+Yuno04@users.noreply.github.com>
2025-04-07 01:13:09 +0700

    Final 1

---
+++ app/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.example.myapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.myapp"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    
+    implementation(platform("androidx.compose:compose-bom:2024.03.00"))// Jetpack Compose BOM (Quản lý phiên bản tự động)
+    implementation("androidx.compose.ui:ui")    // Thư viện UI của Compose
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")    // Material 3 Components
+    implementation("androidx.activity:activity-compose:1.9.0")    // Yêu cầu để sử dụng Jetpack Compose với Activity
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.compose.compiler:compiler:1.5.11")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.9.0")
+
+}
---
+++ app/src/main/java/com/example/myapp/NavItem.kt
@@ -0,0 +1,9 @@
+package com.example.myapp
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class  NavItem(
+    val lable: String,
+    val icon : ImageVector,
+    val badgeCount : Int,
+)
